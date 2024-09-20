c75a757249b18e46b793173a6c6f742cf136a695
a5 <konradek985@gmail.com>
2024-09-20 12:58:32 +0200

    domek zadanie

---
+++ domekZad/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace domekZad
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ domekZad/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace domekZad
+{
+    public partial class MainPage : ContentPage
+    {
+        int likeCount = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void onLikeClicked(object sender, EventArgs e)
+        {
+            likeCount++;
+            lblLike.Text = $"{likeCount} Polubień";
+        }
+
+        private void onDislikeClicked(object sender, EventArgs e)
+        {
+            if (likeCount > 0)
+                likeCount--;
+            lblLike.Text = $"{likeCount} Polubień";
+        }
+    }
+
+}
